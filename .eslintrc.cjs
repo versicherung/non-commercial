160e7c9f378cc72b6aae6d3d1cec7273b22246bc
Jinquan Wang <w1637894214@163.com>
2022-02-20 20:41:31 +0800

    🎉 feat: 更换为 next

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,28 +1,3 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'prettier',
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      experimentalObjectRestSpread: true,
-      jsx: true,
-    },
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  plugins: ['react', '@typescript-eslint'],
-  rules: {
-    'react/display-name': 0,
-    'react/prop-types': 0,
-    'react/react-in-jsx-scope': 0,
-  },
+  extends: 'next/core-web-vitals',
 };
