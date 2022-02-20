64683db83de66ba84b73e9b7a2f0ab341bd5f9b5
Jinquan Wang <w1637894214@163.com>
2022-02-20 21:28:35 +0800

    ✨ feat: 添加less支持

---
+++ next.config.js
@@ -0,0 +1,28 @@
+/** @type {import('next').NextConfig} */
+const path = require('path');
+const withLess = require('next-with-less');
+const withTM = require('next-transpile-modules')([
+  '@arco-design/web-react',
+  '@arco-themes/react-arco-pro',
+]);
+
+module.exports = withLess(
+  withTM({
+    lessLoaderOptions: {
+      lessOptions: {
+        modifyVars: {},
+      },
+    },
+    webpack: (config) => {
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      });
+
+      config.resolve.alias['@'] = path.resolve(__dirname, './src');
+
+      return config;
+    },
+    pageExtensions: ['tsx'],
+  })
+);
